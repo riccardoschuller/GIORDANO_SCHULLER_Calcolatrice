97be9f0846b8d9278b8878a3ecd949f4a069eb91
riccardoschuller <cartuciolo@hotmail.it>
2016-01-02 13:36:58 +0100

    i tasti e le operazioni funzionano

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ if(document.getElementById("display").innerHTML =="0" || document.getElementById
 else{document.getElementById("display").innerHTML+=op;}
 }
 
+
+function risultato() { document.getElementById("display").innerHTML=eval(document.getElementById("display").innerHTML); }
+
+function cancella() { document.getElementById("display").innerHTML=document.getElementById("display").innerHTML.substring(0, document.getElementById("display").innerHTML.length-1); }
+
+function azzera() { document.getElementById("display").innerHTML="0"; }
